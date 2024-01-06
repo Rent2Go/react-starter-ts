e8192741c2ca25857ce7f5ef0e1d739b321e8818
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-01-07 02:06:50 +0300

    created axios productApi

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./homepage.css"
+import axios from 'axios';
 
 type Props = {}
 
 const HomePage = (props: Props) => {
+  useEffect(() => {
+     fetchProducts();
+  }, []);
+
+  const fetchProducts = () => {
+       axios.get("https://dummyjson.com/products");
+  };
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="container">
+      <div className="row">
+        <div className="col-3">
+
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default HomePage
