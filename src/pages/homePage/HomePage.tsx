18296009c68de49037ddc646b1403e51ed2769dd
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-01-07 01:48:22 +0300

    created homePage

---
+++ src/pages/homePage/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./homepage.css"
+
+type Props = {}
+
+export default function HomePage({}: Props) {
+  return (
+    <div>HomePage</div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,1 @@
-export {};
+export {default as HomePage}  from './homePage/HomePage';
