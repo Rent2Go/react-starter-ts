e1617da6e9204f83db845672a1d69d19a7f7b9ef
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-01-07 17:05:50 +0300

    customized add product form

--- src/pages/products/add/AddProduct.tsx
+++ src/pages/products/add/AddProduct.tsx
@@ -1,31 +1,111 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
+
 type Props = {};
 
 const AddProduct = (props: Props) => {
   return (
     <div className="container">
       <div className="main productContainer">
+        <div className="row">
+          <div className="col-12">
+            <h2 className="text-center mt-2 mb-5">Add New Product</h2>
+          </div>
+        </div>
         <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+          <div className="row">
+            <div className="col-md-6 col-sm-12">
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Category Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter Category Name" />
+              </Form.Group>
+            </div>
+            <div className="col-md-6 col-sm-12">
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Brand Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter Brand Name" />
+              </Form.Group>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6 col-sm-12">
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Product Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter Product Name" />
+              </Form.Group>
+            </div>
+            <div className="col-md-6 col-sm-12">
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Description</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Description of Product"
+                />
+              </Form.Group>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6 col-sm-12">
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Discount</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Percentage of Discount"
+                />
+              </Form.Group>
+            </div>
+            <div className="col-md-6 col-sm-12">
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Price</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Price of Products"
+                />
+              </Form.Group>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6 col-sm-12">
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Stock</Form.Label>
+                <Form.Control type="text" placeholder="Enter Stock " />
+              </Form.Group>
+            </div>
+            <div className="col-md-6 col-sm-12">
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Rating</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Rating of Product"
+                />
+              </Form.Group>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Thumbnail</Form.Label>
+                <Form.Control
+                  type="file"
+                  placeholder="Choose a Thumbnail of Product"
+                />
+              </Form.Group>
+            </div>
+          </div>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
+          <div className="row text-center mt-5">
+            <div className="col-md-12 col-sm-12">
+              {" "}
+              <Button variant="primary btn-sm" type="submit">
+                Submit
+              </Button>
+              <Link to="/products" className="btn btn-danger btn-sm">
+                Cancel
+              </Link>
+            </div>
+          </div>
         </Form>
       </div>
     </div>
