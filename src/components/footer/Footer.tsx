e1e6ccfde8b51d3cf4fd01dd36a162e44ea35502
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-01-07 05:23:42 +0300

    init footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,13 @@ const Footer = (): JSX.Element => (
   <footer className="page-footer font-small blue pt-4 bg-dark text-white">
     <div className="footer-copyright text-center py-3">
       © 2024 Copyright:
-      <a href="https://feyzaerat.com.tr/" className="text-white text-decoration-none"> feyzaerat.com.tr</a>
+      <a
+        href="https://feyzaerat.com.tr/"
+        className="text-white text-decoration-none"
+      >
+        {" "}
+        feyzaerat.com.tr
+      </a>
     </div>
   </footer>
 );
