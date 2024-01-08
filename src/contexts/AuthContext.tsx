03e207105e1c137fd677b7263226fd2888cdf83f
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-01-08 13:53:13 +0300

    added authcontext.provider and init to index

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,7 @@
 import { createContext } from "react";
 
-
 export const AuthContext = createContext({});
+
+export const AuthProvider = (props: any) => {
+  return <AuthContext.Provider value={{}}>{props.children}</AuthContext.Provider>;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import "bootstrap/dist/css/bootstrap.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
 
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>
 );
-
-
