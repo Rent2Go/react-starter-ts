724d384aac021e8b20272ac8a60ef5bdfbcf1f1e
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-01-10 14:16:22 +0300

    gave actions to carts

--- src/toolkitStore/slices/cartSlice.ts
+++ src/toolkitStore/slices/cartSlice.ts
@@ -1,13 +1,21 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
-	name: "cart",
-	initialState: {cartItems: []},
-	reducers: {
-		addToCart: (state, action) => {},
-		removeFromCart: (state, action) => {},
-		clearCart: state => {},
-	},
+  name: "cart",
+  initialState: { cartItems: [] as any[] },
+  reducers: {
+    addToCart: (state, action) => {
+      state.cartItems.push(action.payload);
+    },
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter(
+        (i: any) => i.id !== action.payload.id
+      );
+    },
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+  },
 });
 
 export const cartReducer = cartSlice.reducer;
